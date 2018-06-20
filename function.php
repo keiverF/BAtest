11d7c2b20ce692dc6ced334219555262598c1f10
keiver <faneitekeiver@gmail.com>
2018-06-20 05:03:08 +0000

    archivo encargado de manejar peticiones

---
+++ function.php
@@ -0,0 +1,90 @@
+<?php 
+  //incluir modelo 
+    include "postulanteModel.php";
+    class PostulanteFunction
+    {
+    	public $_postu;
+    	public function __construct(){
+    		$this->_postu = new postulanteModel();
+    	}
+         //llamar metodo
+    	public function getMetodo($name,$array = false){
+            return $this->$name($array);
+    	}
+    	//cargar lista de postulantes registrados 
+    	public function load()
+    	{
+             $data = $this->_postu->load();
+    		return $data;
+    	}
+    	public function delete($id){
+            
+            echo $id;
+
+    	}
+    }
+    //instanciar funciones de la clase Postulante function.php
+     $_postuFunt = new PostulanteFunction;   
+
+    //si la peticion es via post
+    if($_SERVER['REQUEST_METHOD'] == 'POST') 
+    {
+        $data1 = array(
+                    ':nombres'               =>$_POST["nombres"],
+                    ':apellidos'             =>$_POST["apellidos"],
+                    ':telefono'              =>$_POST["telefono"],                    
+                    ':id_sexo'               =>$_POST["id_sexo"],
+                    ':id_estado_civil'       =>$_POST["id_estado_civil"],
+                    ':direccion'             =>$_POST["direccion"],
+                    ':dni'                   =>$_POST["dni"],
+                    );
+
+        $data2 = array(':objetivo'           =>$_POST["objetivo"]);
+        $data3 = array(                    
+        	        ':empresa'               =>$_POST["empresa"],
+                    ':actividad_empresa'     =>$_POST["actividad_empresa"],
+                    ':puesto'                =>$_POST["puesto"],
+                    ':nivel'                 =>$_POST["nivel"],
+                    ':id_pais'               =>$_POST["id_pais"],
+                    ':desde'                 =>date("Y-m-d", strtotime($_POST["desde"])),
+                    ':hasta'                 =>date("Y-m-d", strtotime($_POST["hasta"])),
+                    ':area_del_puesto'       =>$_POST["area_del_puesto"],
+                    ':descripcion'           =>$_POST["descripcion"],
+                    ':persona_acargo'       =>$_POST["personas_acargo"],
+                    ':persona_referencia'    =>$_POST["persona_referencia"],
+                    );
+        $data4 = array(
+        	        ':casa_estudios'         =>$_POST["casa_estudios"],
+                    ':nivel'                 =>$_POST["nivelEst"],
+                    ':especialidad'          =>$_POST["especialidad"],
+                    ':desde'                 =>date("Y-m-d", strtotime($_POST["desde_e"])),
+                    ':hasta'                 =>date("Y-m-d", strtotime($_POST["hasta_e"])),
+                    );
+        $data5 = array(
+        	        ':nombre'                =>$_POST["idioma"],
+                    ':oral'                  =>$_POST["oral"],
+                    ':escrito'               =>$_POST["escrito"]
+                    );
+        
+                     $_postuModel = new PostulanteModel;
+        $respuesta = $_postuModel->save($data1,$data2,$data3,$data4,$data5);
+
+        if ($respuesta)
+        {
+        	$msj=['msj'=>'Se guardaron los datos con exito'];
+        }else{
+        	$msj=['msj'=>'Ocurio una falla al guardar los datos'];        	
+        }
+        	echo json_encode($msj);
+        }
+        if($_SERVER['REQUEST_METHOD'] == 'GET') 
+        {   
+
+            $id=$_GET['id'];
+
+            $_postuFunt->getMetodo('delete',$id);
+        }
+
+        //retornar datos de los postulantes en lista
+        $getPostulantes = $_postuFunt->getMetodo('load');
+ ?>
