2b33f35ed4cf86af152faca893cfba42b3c7884a
keiver <faneitekeiver@gmail.com>
2018-06-22 00:06:59 +0000

    modal

---
+++ partials/deletePostuView.php
@@ -0,0 +1,19 @@
+<div class="modal fade" id="deleteModal" tabindex="-1" role="dialog" aria-labelledby="ModalTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="ModalTitle"></h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body text-center" id="body-info">
+
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary ocultar" data-dismiss="modal">Salir</button>
+        <button type="button" class="btn btn-success confirm ocultar">Continuar</button>
+      </div>
+    </div>
+  </div>
+</div>
